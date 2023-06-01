c61559997af8f210e2aae803997ab1b7a9c3437c
OSHIN <izkp44@gmail.com>
2023-06-01 20:57:32 +0700

    Variation #1
    Translations included in Frontend application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import './App.css'
 
 function App() {
-  return <></>
+  const { t, i18n } = useTranslation()
+
+  useEffect(() => {
+    const lng = navigator.language
+    i18n.changeLanguage(lng)
+  }, [])
+
+  const lng = navigator.language
+
+  return (
+    <>
+      <h2>{t('greeting.hello')}</h2>
+      <span>Browser Language: {lng}</span>
+    </>
+  )
 }
 
 export default App
